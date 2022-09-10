153319908b46c0815fc9189e75d74266db6cd725
Aditya verma <87409406+buttkicker3124@users.noreply.github.com>
2022-09-10 16:17:53 +0530

    fontawsome plugin added

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,7 +1,6 @@
 // import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
@@ -10,34 +9,40 @@ function App() {
              <div className="logo h3 col-sm-2">
                 ToursPage
              </div>
-             <ul className="navlists col-sm-7">
+             <ul className="navlists col-sm-8">
                <li className="navlinks">
-                  <a href="index.html" className="links text-decoration-none text-dark h5">
+                  <a href="index.html" className="links text-decoration-none h5">
                      Home
                   </a>
                </li>
                <li className="navlinks">
-                  <a href="index.html" className="links text-decoration-none text-dark h5">
+                  <a href="index.html" className="links text-decoration-none h5">
                      About
                   </a>
                </li>
                <li className="navlinks">
-                  <a href="index.html" className="links text-decoration-none text-dark h5">
+                  <a href="index.html" className="links text-decoration-none h5">
                      Services
                   </a>
                </li>
                <li className="navlinks">
-                  <a href="index.html" className="links text-decoration-none text-dark h5">
+                  <a href="index.html" className="links text-decoration-none h5">
                      Contacts
                   </a>
                </li>
                <li className="navlinks">
-                  <a href="index.html" className="links text-decoration-none text-dark h5">
+                  <a href="index.html" className="links text-decoration-none h5">
                      Ratings
                   </a>
                </li>
              </ul>
+             <div className="col-sm-1 d-flex justify-content-center align-items-center text-white">
+                 {/* <FontAwesomeIcon icon="bars" /> */}
+             </div>
           </nav>
+            <div className="container h1 headline">
+               Explore The World With Us! 
+            </div>
        </header>
     </div>
   );
