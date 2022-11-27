e68fbb9602bdc9f5052d0b09806518b54a73f85f
Aditya verma <87409406+Adityaverma4648@users.noreply.github.com>
2022-11-27 14:49:26 +0530

    imported fontAwesome

--- app1/src/General/Header.js
+++ app1/src/General/Header.js
@@ -1,44 +1,50 @@
-import React from 'react'
+import React from 'react';
+import {FaSearch} from "react-icons/fa"
 
 const Header = () => {
+
+   const stick = (e)=>{
+      var nav = document.querySelector("nav");
+      nav.classList.toggle("top-0","position-fixed")
+   }
+
   return (
     <div className='Header' style={{maxWidth :"100vw"}}>
          <header className="container-fluid">
-          <nav className =" d-flex align-items-center row">
+          <nav className =" d-flex align-items-center row" onScroll={stick}>
              <div className="logo h3 col-sm-2 d-flex justify-content-center align-items-center" style={{height:"60px"}}>
                 ToursPage
              </div>
-             <ul className="navlists d-flex align-items-center justify-content-end px-3 col-sm-10" style={{height:"60px"}}>
+             <ul className="navlists d-flex  flex-wrap align-items-center justify-content-end px-3 col-sm-10" style={{height:"60px"}}>
+               <li className='SearchBtn'>
+                    <FaSearch />
+               </li>
                <li className="navlinks px-3">
                   <a href="index.html" className="links text-decoration-none h5">
-                     Home
+                     aboutUs
                   </a>
                </li>
                <li className="navlinks px-3">
                   <a href="index.html" className="links text-decoration-none h5">
-                     About
+                     ourServices
                   </a>
                </li>
                <li className="navlinks px-3">
                   <a href="index.html" className="links text-decoration-none h5">
-                     Services
+                     ourContacts
                   </a>
                </li>
                <li className="navlinks px-3">
                   <a href="index.html" className="links text-decoration-none h5">
-                     Contacts
-                  </a>
-               </li>
-               <li className="navlinks px-3">
-                  <a href="index.html" className="links text-decoration-none h5">
-                     Ratings
+                     ourRatings
                   </a>
                </li>
              </ul>
-             <div className="col-sm-1 d-flex justify-content-center align-items-center text-white">
-                 {/* <FontAwesomeIcon icon="bars" /> */}
-             </div>
           </nav>
+          <div className="contanier display-1 text-light d-flex align-items-center justify-content-center" style={{height  :"30vh",fontWeight : "600"}}>
+                 Explore The World With Us!
+          </div>
+
        </header>
     </div>
   )
